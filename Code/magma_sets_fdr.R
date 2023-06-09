5f5c34ba04fd6127e3d70ceb2fadd166a7cc07c0
vaqm2 <appadurai.vivek@gmail.com>
2023-06-09 14:33:07 +0200

    debug

--- Code/magma_sets_fdr.R
+++ Code/magma_sets_fdr.R
@@ -31,6 +31,6 @@ for (file in files) {
     gene_sets = rbind(gene_sets, association)
 }
 
-gene_sets %>% mutate(P_ADJ = p.adjust(P, method = c("fdr")))
+gene_sets = gene_sets %>% mutate(P_ADJ = p.adjust(P, method = c("fdr")))
 
 write.table(gene_sets, "GeneSetsFDR.txt", row.names = F, sep = "\t", quote = F)
