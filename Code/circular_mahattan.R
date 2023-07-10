c1123a8c08ccb82ecb53a11bd48441a4480a91af
vaqm2 <appadurai.vivek@gmail.com>
2023-07-10 14:23:40 +0200

    experimenting with radii

--- Code/circular_mahattan.R
+++ Code/circular_mahattan.R
@@ -17,27 +17,97 @@ list_of_assoc = map2(files %>% pull(Path),
     return(this_assoc)
 })
 
+assoc_0.05 = bind_rows(list_of_assoc) %>%
+    filter(P < 0.05) %>%
+    pivot_wider(names_from = Trait, values_from = P)
+
 assoc = bind_rows(list_of_assoc) %>%
     pivot_wider(names_from = Trait, values_from = P)
 
+png(paste0(args[2], "_0.05_Manhattan.png"), 
+    res = 300,
+    width = 15,
+    height = 15,
+    units = "in")
+
 CMplot(assoc, 
        type = "p",
-       plot.type = c("c", "q"),
+       plot.type = "c",
        col = c("grey60", "grey30"),
        outward = FALSE,
        r = 2,
        cir.chr.h = 2,
-       threshold = c(1e-6, 5e-8),
-       threshold.col = c("blue", "red"),
+       multracks = TRUE,
+       threshold = 5e-8,
+       threshold.col = "blue",
        trait.legend.ncol = 7,
-       signal.col = c("red", "green"),
+       signal.line = 1,
+       pch = 21,
+       signal.pch = 24,
+       amplify = FALSE,
+       signal.col = "red",
        trait.legend.pos = "right",
        ylab=expression(-log[10](italic(p))),
-       file = "tiff", 
-       file.name = args[2],
        dpi = 300,
        file.output = TRUE,
-       verbose = TRUE,
        conf.int = TRUE,
        width = 15,
        height = 15)
+
+dev.off()
+
+png(paste0(args[2], "_0.05_Manhattan.png"), 
+    res = 300,
+    width = 15,
+    height = 15,
+    units = "in")
+
+CMplot(assoc, 
+       type = "p",
+       plot.type = "m",
+       col = c("grey60", "grey30"),
+       multracks = TRUE,
+       threshold = c(1e-6, 5e-8),
+       threshold.col = c("green", "blue"),
+       trait.legend.ncol = 7,
+       signal.line = 1,
+       pch = 21,
+       signal.pch = 24,
+       amplify = FALSE,
+       signal.col = "red",
+       trait.legend.pos = "right",
+       ylab=expression(-log[10](italic(p))),
+       dpi = 300,
+       file.output = TRUE,
+       conf.int = TRUE,
+       width = 15,
+       height = 15)
+
+dev.off()
+
+png(paste0(args[2], "_0.05_QQ.png"), 
+    res = 300,
+    width = 10,
+    height = 10,
+    units = "in")
+
+CMplot(assoc, 
+       type = "p",
+       plot.type = "q",
+       signal.pch = c(21:27),
+       signal.cex = 1.2,
+       signal.col = "black",
+       col = c("red", "blue", "green", "yellow", "cyan", "orange", "grey"),
+       threshold = 5e-8,
+       trait.legend.ncol = 7,
+       pch = 21,
+       trait.legend.pos = "right",
+       ylab=expression(-log[10](italic(p))),
+       multracks = TRUE,
+       dpi = 300,
+       file.output = TRUE,
+       conf.int = TRUE,
+       width = 8,
+       height = 8)
+
+dev.off()
